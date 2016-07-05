fdeed8161aaca60afcf2d915db9eade674640435
karengianne <karen.lemaf@gmail.com>
2016-07-05 18:42:20 -0300

    primeiro commit, ja com a classe A

---
+++ src/A.java
@@ -0,0 +1,32 @@
+
+public class A {
+
+	private int A1;
+	
+	private float A2;
+	
+	public int getA1() {
+		return A1;
+	}
+
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+
+	public float getA2() {
+		return A2;
+	}
+
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
